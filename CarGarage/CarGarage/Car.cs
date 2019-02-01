e041ea5c8c44252e307b10b2ec987d10a16dfb37
Joe Scheiman <scheiman.joe@gmail.com>
2019-02-01 15:38:44 -0500

    first commit

---
+++ CarGarage/CarGarage/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGarage
+{
+    public class Car
+    {
+        private readonly int FullTank = 100;
+        public int Speed { get; private set; }
+        public int GasLevel { get; private set; }
+        public bool CarStarted { get; private set; }
+
+        public Car()
+        {
+            Speed = 0;
+            GasLevel = 100;
+            CarStarted = false;
+        }
+        public Car(int startGasLevel)
+        {
+            Speed = 0;
+            GasLevel = startGasLevel;
+            CarStarted = false;
+        }
+
+        public void Accelerate()
+        {
+            Speed += 10;
+            GasLevel -= 10;
+        }
+        public void AddFuel()
+        {
+            GasLevel = FullTank;
+        }
+        public void Brake()
+        {
+            Speed -= 10;
+            if (Speed < 0)
+            {
+                Speed = 0;
+            }
+        }
+        public void ToggleEngine()
+        {
+            CarStarted = !CarStarted;
+        }
+    }
+}
---
+++ CarGarage/CarGarage/Garage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGarage
+{
+    public class Garage
+    {
+        public List<Car> TheGarage;
+
+        public Garage()
+        {
+            TheGarage = new List<Car>();
+
+        }
+        public void AddCar()
+        {
+            //Car newCar = new Car();
+            this.TheGarage.Add(new Car());
+
+        }
+
+        
+        
+    }
+}
